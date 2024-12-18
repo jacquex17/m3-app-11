8038bbc45c524100d8fc24a814877c55922a8621
jacquevn321@gmail.com <jacquevn321@gmail.com>
2024-12-18 16:28:45 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,11 +1,14 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+from anvil.js import get_dom_node
+
 
 
 class Form1(Form1Template):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
+        self.text_box_1.type = "search"
         self.init_components(**properties)
         print("hello")
-
+        get_dom_node(self.text_box_1).type='search'
         # Any code you write here will run before the form opens.
