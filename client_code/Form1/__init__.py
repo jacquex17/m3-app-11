432c3122cc0d1e1cb364c3507c0986b53c66d79b
Jacque van Niekerk <jvanniekerk@cfc.com>
2025-01-08 11:12:27 +0000

    change again

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,6 +9,6 @@ class Form1(Form1Template):
         # Set Form properties and Data Bindings.
         self.text_box_1.type = "search"
         self.init_components(**properties)
-        print("helloabc")
+        print("helloabdc")
         get_dom_node(self.text_box_1).type='search'
         # Any code you write here will run before the form opens.
